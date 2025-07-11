021bfbe6eb07023a9042177bb850134cfbbe37cc
Craig Ciquera <craig.ciquera@gmail.com>
2025-07-11 11:48:56 -0400

    Delete types.go

--- types.go
+++
@@ -1,9 +0,0 @@
-package cmd
-
-type ScanResult struct {
-	Source     string `json:"source"`     // e.g., terraform
-	Resource   string `json:"resource"`   // e.g., aws_instance.web
-	ChangeType string `json:"changeType"` // e.g., update
-	Severity   string `json:"severity"`   // info, warning, critical
-	Message    string `json:"message"`
-}
