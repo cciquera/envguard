428c95739a5bb7a7c0897253954527ff8f7b8144
Craig Ciquera <cciquera@mathworks.com>
2025-07-11 12:34:52 -0400

    fix imports in main

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/cciquera/envguard/cmd"
 )
 
-var version = "dev"
+var version = "0.1.1"
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "version" {
-        fmt.Println("envguard version:", version)
-        return
-    }
+		fmt.Println("envguard version:", version)
+		return
+	}
 	cmd.Execute()
 }
